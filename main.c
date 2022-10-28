da10bb6c8a9f99fba52cb294ca5176c358963524
Patrick amusivwa <patrick.amusivwa@students.jkuat.ac.ke>
2022-10-28 12:18:12 +0300

    update files

---
+++ main.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+double sum(int num1,int num2 ){
+    return result;
+    result=num1 + num2;
+}
+int main(){
+    int num1,num2;
+    printf("enter two numbers ");
+    scanf("%d %d",&num1&num2);
+    printf("your sum is %d",sum(num1,num2));
+    return 0;
+
+}
